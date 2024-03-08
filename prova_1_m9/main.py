360a8248a2ad72b88c1076ac5268c5585c947304
cmtabr <caio.abreu@sou.inteli.edu.br>
2024-03-08 11:11:09 -0300

    feat(final-code): :sparkles: last part written for topic listen

--- prova_1_m9/main.py
+++ prova_1_m9/main.py
@@ -12,34 +12,21 @@ class BrokerConnection:
     def publish(self, topic: str, message: Any, qos: int) -> None:
         self.client.publish(topic, payload=message, qos=qos, retain=False)
 
+    def on_message(client, userdata, message):
+        incoming_data = message.payload.decode('utf-8')
+        if incoming_data["tipo"] == "freezer" and incoming_data["temperatura"] > -15:
+            print(f'[Alerta] : Temperatura elevada - Setor Congelados | {incoming_data["temperatura"]}ºC')
+        elif incoming_data["tipo"] == "freezer" and incoming_data["temperatura"] < -25:
+            print(f'[Alerta] : Temperatura baixa - Setor Congelados | {incoming_data["temperatura"]}ºC')
+        elif incoming_data["tipo"] == "geladeira" and incoming_data["temperatura"] > 10:
+            print(f'[Alerta] : Temperatura elevada - Setor Refrigerados | {incoming_data["temperatura"]}ºC')
+        elif incoming_data["tipo"] == "geladeira" and incoming_data["temperatura"] < 2:
+            print(f'[Alerta] : Temperatura baixa - Setor Refrigerados | {incoming_data["temperatura"]}ºC')
+        else:
+            print(f'Nenhuma ação necessária')
+
     def __del__(self):
         self.client.disconnect()
-
-# class PayloadGenerator: 
-#     def __init__(self) -> None: 
-#         self.fake = Faker()
-
-#     def generate_id():
-#         for i in range(1, 10, 1):
-#             for j in range(i - 2):
-#                 for k in range(j - 1):
-#                     l = ('f' if k % 2 == 0 else 'g')
-#                     yield f'lj0{i}{l}0{j}'
-#                 time.sleep(1)
-
-#     def fake_sensor_data(self) -> json:
-#         for i in self.generate_id:
-#             id = i 
-#             type = ('geladeira' if id[4] == 'g' else 'freezer')
-#             date = time.strftime('%Y-%m-%d %H:%M:%S')
-#             temperature = self.fake.pyint(min_value=-30, max_value=-5)
-#             sensor = {
-#                 "id":id,
-#                 "type":type,
-#                 "date": date,
-#                 "temperature": temperature
-#             }
-#         return json.dumps(sensor)
 
 def generate_id():
         for i in range(1, 10, 1):
@@ -51,7 +38,7 @@ def generate_id():
 
 if __name__ == '__main__':
     fake = Faker()
-    broker = BrokerConnection("R2-D2")
+    broker = BrokerConnection('R2-D2')
     for i in generate_id():
         id = i 
         tipo = 'geladeira' if id[4] == 'g' else 'freezer'
@@ -63,5 +50,5 @@ if __name__ == '__main__':
             "date": date,
             "temperatura": temperatura
         }
-        print(sensor)
-        broker.publish("data/mics6814", str(sensor), 0)
+        broker.publish("prova/m9-p1", json.dumps(sensor), 1)
+    broker.on_message
